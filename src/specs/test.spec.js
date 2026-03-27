459caf4b864ae044d3fa090e8191de04a4c308d3
Lőrincz Emma <lorinczemma03@gmail.com>
2026-03-27 13:04:03 +0100

    Conf file changes and adding assertions to task1

--- src/specs/test.spec.js
+++ src/specs/test.spec.js
@@ -1,26 +1,30 @@
-
 describe("UC-1 Sorting Validation - Login Page and sorting", () => {
   it("should let you log in and sorts the price (low to high)", async () => {
     await browser.url('/');
 
     const usernameInput = $('//input[@placeholder="Username"]');
-    await usernameInput.setValue('standard_user');
-
     const passwordInput = $('//input[@placeholder="Password"]');
-    await passwordInput.setValue('secret_sauce');
-
     const loginButton = $('//input[@value="Login"]');
+    
+    await expect(usernameInput).toBeDisplayed();
+    await expect(passwordInput).toBeDisplayed();
+    await expect(loginButton).toBeDisplayed();
+
+    await usernameInput.setValue('standard_user');
+    await passwordInput.setValue('secret_sauce');
     await loginButton.click();
 
     const sortDropdown = $('//select[@class="product_sort_container"]');
+    await expect(sortDropdown).toBeDisplayed();
     await sortDropdown.selectByVisibleText('Price (low to high)');
 
     const priceElements = await $$('//div[@data-test="inventory-item-price"]');
+    await expect(priceElements.length).toBeGreaterThan(0);
 
     const prices = [];
     for (const element of priceElements) {
-    const text = await element.getText();
-    prices.push(parseFloat(text.replace('$', '')));
+      const text = await element.getText();
+      prices.push(parseFloat(text.replace('$', '')));
     }
 
     for (let i = 0; i < prices.length - 1; i++) {
